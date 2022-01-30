2dca60ae8be201e180e2d3e8144493323aaf8165
Alex <2hala2real2madrid2@gmail.com>
2022-01-30 21:00:36 +0300

    Maket

---
+++ JavaMySQLApplication/src/main/java/com/example/javamysqlapplication/HelloController.java
@@ -0,0 +1,31 @@
+package com.example.javamysqlapplication;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+
+    @FXML
+    private TextField breadth;
+
+    @FXML
+    private TextField depth;
+
+    @FXML
+    private CheckBox full;
+
+    @FXML
+    private Button go;
+
+    @FXML
+    private CheckBox top;
+
+    @FXML
+    private TextField url;
+
+    @FXML
+    private TextField words;
+
+}
---
+++ JavaMySQLApplication/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.javamysqlapplication {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.javamysqlapplication to javafx.fxml;
+    exports com.example.javamysqlapplication;
+}
